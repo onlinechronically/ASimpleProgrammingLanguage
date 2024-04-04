d13733f0c4548d026246c1476ab0d631af5afeb9
lillie <onlinechronically@protonmail.com>
2024-04-04 19:28:28 -0400

    add: ProgramNode

--- src/ProgramNode.java
+++ src/ProgramNode.java
@@ -4,15 +4,31 @@ final class ProgramNode extends AstNode {
     super(args);
   }
 
+  /**
+   * Evaluate a sequence of statements, in a given Environment
+   * @param env The given Environment
+   * @return The resulting AstNode
+   */
   @Override
   AstNode eval(Environment env) {
-    // TODO.
-    throw new UnsupportedOperationException("not implemented yet");
+    AstNode lastNode = null;
+    for (AstNode child : getChildren()) {
+      if (child instanceof DefNode) {
+        lastNode = null;
+        child.eval(env);
+      } else {
+        lastNode = child.eval(env);
+      }
+    }
+    return lastNode;
   }
 
+  /**
+   * Get a String representation of a sequence of statements
+   * @return The resulting String
+   */
   @Override
   public String toString() {
-    // TODO.
-    throw new UnsupportedOperationException("not implemented yet");
+    return String.join("\n", this.getChildren().stream().map(AstNode::toString).toList());
   }
 }
