97538b255f9a7e012275c869f9ca2d4716e002df
lillie <onlinechronically@protonmail.com>
2024-04-09 10:33:45 -0400

    add: FuncNode

--- src/FuncNode.java
+++ src/FuncNode.java
@@ -1,19 +1,25 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 final class FuncNode extends AstNode {
+  public List<String> getIdentifiers() {
+    return identifiers;
+  }
+
+  private List<String> identifiers;
 
   FuncNode(List<String> identifiers, AstNode body) {
-    // TODO.
-    throw new UnsupportedOperationException("not implemented yet");
+    super(body);
+    this.identifiers = identifiers;
   }
 
   @Override
   AstNode eval(Environment env) {
-    throw new UnsupportedOperationException("not implemented yet");
+    return this.getChildren().get(0).eval(env);
   }
 
   @Override
   public String toString() {
-    throw new UnsupportedOperationException("not implemented yet");
+    return String.format("(λ (%s) %s)", String.join(", ", identifiers), this.getChildren().get(0));
   }
 }
